44572ada8c879fd11fccc15395d0478d73134a5a
Adinnu Benedict <24754078+BenQoder@users.noreply.github.com>
2023-03-29 01:25:08 +0100

    Files Zipped

--- src/Commands/LaravelDeployCommand.php
+++ src/Commands/LaravelDeployCommand.php
@@ -2,17 +2,64 @@
 
 namespace BenQoder\LaravelDeploy\Commands;
 
+use ZipArchive;
+use ZanySoft\Zip\Facades\Zip;
+use RecursiveIteratorIterator;
 use Illuminate\Console\Command;
+use RecursiveDirectoryIterator;
+use Illuminate\Support\Facades\File;
 
 class LaravelDeployCommand extends Command
 {
-    public $signature = 'laravel-deploy';
+    public $signature = 'deploy';
 
-    public $description = 'My command';
+    public $description = 'Deploy Project';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $this->info('Preparing to deploy project...');
+
+        File::deleteDirectory(base_path('debloyables'));
+        File::ensureDirectoryExists(base_path('debloyables'));
+
+        $zip = Zip::create(base_path('debloyables/source.zip'), true);
+
+        foreach (scandir(base_path()) as $item) {
+            if (in_array($item, [
+                ".",
+                "..",
+                "vendor",
+                "node_modules",
+            ])) continue;
+
+            $zip->add(
+                base_path($item),
+            );
+        }
+
+        $zip->close();
+
+        $this->info('Preparing vendor!');
+
+        $rootPath = base_path('vendor');
+
+        $zip = new ZipArchive();
+        $zip->open(base_path('debloyables/vendor.zip'), ZipArchive::CREATE | ZipArchive::OVERWRITE);
+
+        $files = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($rootPath),
+            RecursiveIteratorIterator::LEAVES_ONLY
+        );
+
+        foreach ($files as $name => $file) {
+            if (!$file->isDir()) {
+                $filePath = $file->getRealPath();
+                $relativePath = substr($filePath, strlen($rootPath) + 1);
+                $zip->addFile($filePath, $relativePath);
+            }
+        }
+
+        $zip->close();
 
         return self::SUCCESS;
     }
